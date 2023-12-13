07c6a3616e91355a8af944f4c2c1bd3db1a671cd
VadimStrashnov <strvad2005@gmail.com>
2023-12-14 02:22:02 +0300

    Изменил 3 лабу

--- laboratory-task-3/lab3.cpp
+++ laboratory-task-3/lab3.cpp
@@ -9,39 +9,49 @@
 
 #include <iostream>
 #include <cmath>
+#include <exception>
 
 
-void inputNumber(int& number) 
+void inputNumber(int32_t& number) 
 {
-  std::cout << " Write number: " << '\n';
+  std::cout << " Write number:\n";
   std::cin >> number;
 }
 
 
-void writeElem(int number, double arr[])
+void writeElem(int32_t number, double arr[])
 {
-  for (int i = 0; i < number; i++)
+  for (int32_t i = 0; i < number; i++)
   {
     std::cin >> arr[i];
   }
 }
 
 
-void inputMode(int& mode)
+void inputMode(int32_t& mode)
 {
-  std::cout << " choose elements ( press 1 ) or random elements ( press 2 ) " << '\n';
+  std::cout << " choose elements ( press 1 ) or random elements ( press 2 )\n";
   std::cin >> mode;
 }
 
 
-void inputBorder(int& border)
+void inputBorder(int32_t& border)
 {
-  std::cout << " Input min " << '\n';
+  std::cout << " Input min:\n";
   std::cin >> border;
 }
 
 
-void randomElements(int number, double  arr[], int lowBorder, int highBorder)
+void checkBorders(int32_t lowborder, int32_t highborder)
+{
+  if (lowBorder > highBorder)
+      {
+        std::swap(lowBorder, highBorder);
+      }
+}
+
+
+void randomElements(int32_t number, double  arr[], int32_t lowBorder, int32_t highBorder)
 {
   for (size_t i = 0; i < number; ++i)
   {
@@ -50,21 +60,43 @@ void randomElements(int number, double  arr[], int lowBorder, int highBorder)
 }
 
 
-void primalArray(int number, double arr[])
+void menu(int32_t number, double  arr[], int32_t lowBorder, int32_t highBorder, int32_t mode)
 {
-  std::cout << " Current array: ";
-  for (int index = 0; index < number; ++index)
+  switch (mode)
+    { 
+    case 1:
+      std::cout << " Write array elements:\n";
+      writeElem(number, arr);
+      break;
+    case 1:
+      std::cout << " Write diapazon:\n";
+      inputBorder(lowBorder);
+      inputBorder(highBorder);
+      checkBorders(lowborder, highborder);
+      randomElements(number, arr, lowBorder, highBorder);
+      break;
+    default:
+      throw " Wrong command!\n ";
+      break;
+    }
+}
+
+
+void primalArray(int32_t number, double arr[])
+{
+  std::cout << " Current array:\n ";
+  for (int32_t i = 0; i < number; ++i)
   {
-    std::cout << arr[index] << " ";
+    std::cout << arr[i] << " ";
   }
   std::cout << '\n';
 }
 
 
-double max(int number, double arr[])
+double max(int32_t number, double arr[])
 {
   double max = arr[0];
-  for (int i = 0; i < number; ++i)
+  for (int32_t i = 0; i < number; ++i)
   {
     if (max < arr[i])
     {
@@ -75,11 +107,11 @@ double max(int number, double arr[])
 }
 
 
-int countLength(int number, double arr[])
+int32_t countLength(int32_t number, double arr[])
 {
-  int currentLength = 1;
-  int maxLength = 1;
-  for (int i = 1; i < number; ++i)
+  int32_t currentLength = 1;
+  int32_t maxLength = 1;
+  for (int32_t i = 1; i < number; ++i)
   {
     if (arr[i] != arr[i - 1])
     {
@@ -102,10 +134,10 @@ int countLength(int number, double arr[])
 }
 
 
-int negative(int number, double arr[])
+int32_t negative(int32_t number, double arr[])
 {
-  int lastNegative = -1;
-  for (int i = 0; i < number; ++i)
+  int32_t lastNegative = -1;
+  for (int32_t i = 0; i < number; ++i)
   {
     if (arr[i] < 0)
     {
@@ -116,41 +148,46 @@ int negative(int number, double arr[])
 }
 
 
-double sumAfterNegative(int number, double arr[], int lastnegative)
+double sumAfterNegative(int32_t number, double arr[], int32_t lastnegative)
 {
   double sum = 0;
-  for (int index = lastnegative + 1; index < number; ++index)
+  for (size_t i = lastnegative + 1; i < number; ++i)
   {
-    sum += floor(arr[index]);
+    sum += floor(arr[i]);
   }
   return sum;
 }
 
 
-void conditionSumAfterNegative(int number, double arr[])
+void conditionSumAfterNegative(int32_t number, double arr[])
 {
+  if (negative(number, arr) == number - 1)
+  {
+    std::cout << " Summa does not exist!\n ";
+    break;
+  }
   if (negative(number, arr) != -1)
   {
     std::cout << " Summa after last negative: " << sumAfterNegative(number, arr, negative(number, arr)) << '\n';
   }
   else
   {
-    std::cout << " No negative elements " << '\n';
+    std::cout << " No negative elements\n";
   }
 }
 
 
-void conditionSort(int number, double arr[])
+void conditionSort(int32_t number, double arr[])
 {
-  int count = 0;
-  for (int i = 0; i <= number; ++i)
+  int32_t count = 0;
+  for (size_t i = 0; i <= number; ++i)
   {
-    int count = 0;
+    int32_t count = 0;
     if (max(number, arr) >= 0)
     {
       if (arr[i] >= max(number, arr) * 0.8)
       {
-        for (int j = i; j > count; --j)
+        for (size_t j = i; j > count; --j)
         {
           std::swap(arr[j - 1], arr[j]);
         }
@@ -161,7 +198,7 @@ void conditionSort(int number, double arr[])
     {
       if (arr[i] >= max(number, arr) * 1.2)
       {
-        for (int j = i; j > count; --j)
+        for (size_t j = i; j > count; --j)
         {
           std::swap(arr[j - 1], arr[j]);
         }
@@ -172,60 +209,46 @@ void conditionSort(int number, double arr[])
 }
 
 
-void completeArray(int& number, double arr[])
+void completeArray(int32_t& number, double arr[])
 {
-  std::cout << " Formed array: ";
-  for (int index = 0; index < number; ++index)
+  std::cout << " Formed array:\n ";
+  for (size_t i = 0; i < number; ++i)
   {
-    std::cout << arr[index] << " ";
+    std::cout << arr[i] << " ";
   }
 }
 
 
 int main()
 {
-  int number = 0, mode = 0, highBorder = 0, lowBorder = 0;
+  try
+  {
+  int32_t number = 0;
+  int32_t lowBorder = 0;
+  int32_t highBorder = 0;
+  int32_t mode = 0;
   double arr[1000] = { 0 };
+
   srand(time(0));
   inputNumber(number);
   if (number < 1)
   {
-    std::cout << " Uncorrect number " << '\n';
+    throw " Wrong number!\n ";
   }
   else
   {
     inputMode(mode);
-    switch (mode)
-    { 
-    case 1:
-      std::cout << " Write array elements: " << '\n';
-      writeElem(number, arr);
-      break;
-    case 2:
-      std::cout << " Write diapazon: " << '\n';
-      inputBorder(lowBorder);
-      inputBorder(highBorder);
-      if (lowBorder > highBorder)
-      {
-        std::swap(lowBorder, highBorder);
-      }
-        randomElements(number, arr, lowBorder, highBorder);
-
-      break;
-    default:
-      std::cout << " Wrong command " << '\n';
-      return false;
-      break;
-    }
+    menu(number, arr, lowBorder, highBorder, mode);
     primalArray(number, arr);
-
     std::cout << " The longest: " << countLength(number, arr) << '\n';
-
     conditionSumAfterNegative(number, arr);
-
     conditionSort(number, arr);
     completeArray(number, arr);
   }
+  }
+  catch (const char* msg)
+  {
+    std::cout << msg;
+  }
   return 0;
 }
-
