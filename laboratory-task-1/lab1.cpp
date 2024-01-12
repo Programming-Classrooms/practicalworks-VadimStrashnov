83af9f17960d989b67548ab5fea4373492afdd6e
VadimStrashnov <strvad2005@gmail.com>
2024-01-12 14:16:21 +0300

    Готова к разносу

--- laboratory-task-1/lab1.cpp
+++ laboratory-task-1/lab1.cpp
@@ -1,71 +1,99 @@
+/*  Разработать консольное приложение для вычисления приближённого значения функции, 
+    используя представление ее в виде ряда Тейлора.
+    Вычисления заканчивать, когда очередное слагаемое окажется по модулю меньше заданного числа epsilon,
+    где 0 < epsilon <= 10-k , k – натуральное число, k>1. Сравнить полученный результат со значением,
+    вычисленным с помощью стандартных функций. Значение x и k ввести по запросу.
+*/
+
+
 #include <iostream>
 #include <cmath>
-#include <exception>
 
 
-template <typename T>
-void inputValue(T& value)
+void inputNumber(double& number)
 {
-    std::cin >> value;
+  std::cout << " Write number: " << '\n';
+  std::cin >> number;
 }
 
 
-void checkNumber(const double number)
+void checkNumber(double number)
 {
-    if (number <= -1 || number > 1){
-        throw " Wrong number! ";
-    }
+  if (number <= -1 || number > 1)
+  {
+    throw std::exception (" Wrong number! ");
+  }
 }
 
 
-void checkAccuracy(const size_t accuracy)
+void inputAccuracy(size_t& accuracy)
 {
-    if (accuracy <= 1){
-        throw " Wrong accuracy! ";
-    }
+  std::cout << " Write accuracy: " << '\n';
+  std::cin >> accuracy;
 }
 
 
-void checkEpsilon(const double epsilon, const size_t accuracy)
+void checkAccuracy(size_t accuracy)
 {
-    if (epsilon <= 0 || epsilon >= pow(10, accuracy)){
-        throw " Wrong epsilon! ";
-    }
+  if (accuracy <= 1)
+  {
+    throw std::exception (" Wrong accuracy! ");
+  }
+}
+
+
+void inputEpsilon(double& epsilon)
+{
+  std::cout << " Write epsilon: " << '\n';
+  std::cin >> epsilon;
+}
+
+
+void checkEpsilon(double epsilon, size_t accuracy)
+{
+  if (epsilon <= 0 || epsilon >= pow(10, accuracy))
+  {
+    throw std::exception (" Wrong epsilon! ");
+  }
 }
 
 
 double sum(double epsilon, double number)
 {
-    double sum = 0, term = number;
-    for (size_t i = 2; abs(term) > epsilon; ++i){
-        sum += term;
-        term *= (-1 * number * (i - 1)) / i;
-    }
-    return sum;
+  double sum = 0;
+  double term = number;
+
+  for (size_t i = 2; abs(term) > epsilon; ++i)
+  {
+    sum += term;
+    term *= ( - 1 * number * (i - 1)) / i;
+  }
+
+  return sum;
 }
 
 
 int main()
 {
-    try
-    {
-        size_t accuracy = 0;
-        double epsilon = 0;
-        double number = 0;
+  try
+  {
+    size_t accuracy = 0;
+    double epsilon = 0;
+    double number = 0;
 
-        std::cout << " Write number:\n";
-        inputValue(number);
-        checkNumber(number);
-        std::cout << " Write accuracy:\n";
-        inputValue(accuracy);
-        checkAccuracy(accuracy);
-        std::cout << " Write epsilon:\n";
-        inputValue(epsilon);
-        checkEpsilon(epsilon, accuracy);
-        std::cout << sum(epsilon, number) << '\n' << log(number + 1) << " = " << sum(epsilon, number);
-    }
-    catch (const char* msg)
-    {
-        std::cout << msg;
-    }
+    inputNumber(number);
+    checkNumber(number);
+    inputAccuracy(accuracy);
+    checkAccuracy(accuracy);
+    inputEpsilon(epsilon);
+    checkEpsilon(epsilon, accuracy);
+
+    std::cout << '\n' << log(number + 1) << " = " << sum(epsilon, number);
+  }
+  catch (std::exception e)
+  {
+    std::cout << e.what() << '\n';
+  }
+
+  return 0;
 }
