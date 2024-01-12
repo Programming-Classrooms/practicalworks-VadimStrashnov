b2d38213428a113e8ecfa7c0d666e9ccb4a1d72e
VadimStrashnov <strvad2005@gmail.com>
2024-01-12 14:45:55 +0300

    Изменил описание и добавил исключения

--- laboratory-task-5/lab5.cpp
+++ laboratory-task-5/lab5.cpp
@@ -1,4 +1,28 @@
-//Пример #12 на фото
+/*  Вычислить значение определенного интеграла с аналитически заданной подынтегральной
+    функцией с заданной точностью eps
+    А) по формуле левых прямоугольников;
+    Б) по формуле правых прямоугольников;
+    В) по формуле средних прямоугольников;
+    Г) по формуле трапеций;
+    Д) по формуле Симпсона (параболических трапеций).
+
+    Формула средних прямоугольников:
+    ∫f(x)dx ≈ h*[f(x 1 )+ f(x 2 )+…+ f(x n )], где h=(b-a)/n, f(x i )=a+i*h-h/2,
+
+    где n–число точек деления отрезка [a, b]. Для вычисления первого приближения можно взять
+    n=4. Чтобы оценить точность, с которой вычислено значение интеграла, необходимо найти второе
+    приближение. Для этого можно увеличить n в два раза. Если s 1 и s 2 – два соседних приближения и |s 1
+    – s 2 |&lt;eps, то точность считается достигнутой и s 2 принимается за искомое значение интеграла. В
+    противном случае надо положить s 1 = s 2 и удвоить число точек деления отрезка [a, b]. После этого
+    вычисляется новое значение s 2 . Процесс удвоения n и вычисления s 2 надо продолжать до тех пор,
+    пока модуль разности s 1 и s 2 не станет меньше eps. Остальные формулы посмотреть самим.
+
+    Разработать функции, реализующие методы вычислений интеграла, входными параметрами
+    которых должны быть функция для вычисления значения подынтегрального выражения, пределы
+    интегрирования и точность вычислений. Головная программа должна вывести результат вычислений
+    для записанных в задании интегралов по заданным формулам и значение n, при котором достигнута
+    заданная точность. Для проверки вычислений в таблице приведены значения интегралов.
+*/
 
 
 #include <exception>
@@ -17,7 +41,7 @@ void checkEpsilon(const double eps, const  double lowborder, const  double highb
 {
   if (eps <= lowborder || eps >= highborder)
   {
-    throw " Wrong epsilon!\n";
+    throw std::exception (" Wrong epsilon!\n");
   }
 }
 
@@ -25,6 +49,7 @@ void checkEpsilon(const double eps, const  double lowborder, const  double highb
 double step(const double lowborder, const double highborder, const size_t counter)
 {
   double step = (highborder - lowborder) / counter;
+
   return step;
 }
 
@@ -32,6 +57,7 @@ double step(const double lowborder, const double highborder, const size_t counte
 double theFirstFunction(const double number)
 {
   double result = sin(number * 0.4) * pow(number, 0.23);
+
   return result;
 }
 
@@ -39,6 +65,7 @@ double theFirstFunction(const double number)
 double theSecondFunction(const double number)
 {
   double result = exp(-1 * pow(number, 2)) * pow(sin(0.4 * number), 2) / (pow(number, 2) + 3.5);
+
   return result;
 }
 
@@ -46,6 +73,7 @@ double theSecondFunction(const double number)
 double theThirdFunction(const double number)
 {
   double result = exp(1.1 * number / (pow(number, 2) + 1)) / (pow(number, 2) + 1.1);
+
   return result;
 }
 
@@ -57,16 +85,19 @@ double formulaTheAverageRectangle(const double eps, const double lowborder, cons
   double lastSumOfIntegrals = -1;
   double h = 0;
   double x = 0;
+
   while (fabs(sumOfIntegrals - lastSumOfIntegrals) >= eps) {
     lastSumOfIntegrals = sumOfIntegrals;
     sumOfIntegrals = 0;
     counter *= 2;
     h = step(lowborder, highborder, counter);
+
     for (size_t i = 1; i < counter; ++i) {
       x = lowborder + i * h - h / 2;
       sumOfIntegrals += h * function(x);
     }
   }
+
   return sumOfIntegrals;
 }
 
@@ -76,15 +107,18 @@ double formulaTheAverageTrapezoid(const double eps, const  double lowborder, con
   double sumOfIntegrals = 0;
   double lastSumOfIntegrals = -1;
   size_t counter = 4;
+
   while (fabs(sumOfIntegrals - lastSumOfIntegrals) >= eps){
     counter *= 2;
     double h = step(lowborder, highborder, counter);
     lastSumOfIntegrals = sumOfIntegrals;
     sumOfIntegrals = h * (function(lowborder) + function(highborder)) / 2;
+
     for (double i = h; i < highborder; i += h){
       sumOfIntegrals += h * function(i);
     }
   }
+
   return sumOfIntegrals;
 }
 
@@ -96,14 +130,16 @@ int main()
     double lowborder = 0;
     double highborder = 1;
 
+
     inputEpsilon(eps);
     checkEpsilon(eps, lowborder, highborder);
     std::cout << " First result:\n" << formulaTheAverageRectangle(eps, lowborder, highborder, theFirstFunction) << '\n' << formulaTheAverageTrapezoid(eps, lowborder, highborder, theFirstFunction) << '\n';
     std::cout << " Second result:\n" << formulaTheAverageRectangle(eps, lowborder, highborder, theSecondFunction) << '\n' << formulaTheAverageTrapezoid(eps, lowborder, highborder, theSecondFunction) << '\n';
     std::cout << " Third result:\n" << formulaTheAverageRectangle(eps, lowborder, highborder, theThirdFunction) << '\n' << formulaTheAverageTrapezoid(eps, lowborder, highborder, theThirdFunction) << '\n';
   }
-  catch (const char* msg){
-    std::cout << msg;
+  catch (std::exception e) {
+    std::cout << e.what();
   }
+
   return 0;
 }
