a05c2cd4c25d4480f583897f433bb91bb86a16ae
VadimStrashnov <strvad2005@gmail.com>
2024-01-12 15:06:25 +0300

    Исправил недостатки 8 лабы

--- laboratory-task-8/lab8.cpp
+++ laboratory-task-8/lab8.cpp
@@ -1,4 +1,16 @@
-//Условие #12 на фото
+/*  Словом, входящим в состав строки, назовём непрерывную
+    последовательность символов, ограниченную символами–разделителями, началом и концом строки. 
+    Символы, относящиеся к разделителям, определяет пользователь, если иное не сказано в условии задачи.
+    Если решить задачу невозможно, программа должна выдать соответствующее сообщение.
+
+    Словом в строке считается последовательность букв латинского
+    алфавита, остальные символы рассматриваются как разделители между словами.
+    Слова в каждой из двух исходных строк упорядочены по алфавиту.
+    Необходимо сформировать новую строку из всех слов исходных строк. 
+    Слова в новой строке должны быть также упорядочены по алфавиту. 
+    Прописные и строчные буквы в словах не различать. 
+    Слова в новой строке должны разделяться ровно одним пробелом.
+*/
 
 
 #include <iostream>
@@ -36,12 +48,14 @@ void turnDelimetersIntoSpace(std::string string)
 void checkForWords(const std::string string)
 {
   size_t counter = 0;
+
   for (size_t i = 0; i < string.length(); ++i) {
     if (isalpha(string[i]))
     {
       ++counter;
     }
   }
+
   if (counter < 1)
   {
     throw std::exception(" No words in string!\n");
@@ -52,6 +66,7 @@ void checkForWords(const std::string string)
 bool caseInsensitiveCompare(const std::string firstString, const std::string secondString)
 {
   size_t length = std::min(firstString.length(), secondString.length());
+
   for (size_t i = 0; i < length; ++i) {
     char symbol1 = std::tolower(firstString[i]);
     char symbol2 = std::tolower(secondString[i]);
@@ -59,6 +74,7 @@ bool caseInsensitiveCompare(const std::string firstString, const std::string sec
       return symbol1 < symbol2;
     }
   }
+
   return firstString.length() < secondString.length();
 }
 
@@ -79,21 +95,27 @@ void sortStringAlphabetically(const std::string firstString, const  std::string 
 {
   size_t prev = 0;
   size_t pos = 0;
+
   while ((pos = firstString.find_first_of(" ", prev)) != std::string::npos)
   {
     if (pos > prev)
       manyOfWords.push_back(firstString.substr(prev, pos - prev));
     prev = pos + 1;
   }
-  if (prev < firstString.length())
+
+  if (prev < firstString.length()) {
     manyOfWords.push_back(firstString.substr(prev, std::string::npos));
+  }
+
   prev = 0;
   pos = 0;
+
   while ((pos = secondString.find_first_of(" ", prev)) != std::string::npos){
     if (pos > prev)
       manyOfWords.push_back(secondString.substr(prev, pos - prev));
     prev = pos + 1;
   }
+
   if (prev < secondString.length()) {
     manyOfWords.push_back(secondString.substr(prev, std::string::npos));
   }
@@ -120,18 +142,22 @@ int main()
     checkIsEmptyString(firstString);
     checkForWords(firstString);
     turnDelimetersIntoSpace(firstString);
+
     std::cout << " Write second string:\n";
     inputString(secondString);
     checkIsEmptyString(secondString);
     checkForWords(secondString);
     turnDelimetersIntoSpace(secondString);
+
     std::vector<std::string> manyOfWords;
     sortStringAlphabetically(firstString, secondString, manyOfWords);
+
     std::cout << " Your result:\n";
     outputFinalString(manyOfWords);
   }
   catch (const std::exception e){
     std::cout << e.what();
   }
+
   return 0;
 }
